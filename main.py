0de836b0fdaad087a1ca555b586caff99a38a554
Luciano Filho <lvgalvaofilho@gmail.com>
2023-07-22 05:08:30 -0300

    feat: add path /createposts

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data" : "This is your posts"}
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "sucessfully created posts"}
