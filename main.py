469136daafb8cc36a710b6346e3a843ea956de9d
Luciano Filho <lvgalvaofilho@gmail.com>
2023-07-23 08:09:46 -0300

    feat: add my_posts list

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of posts1",
+            "content": "content of post1",
+            "id": 1},
+            {"title": "title of posts2",
+            "content": "content of post2",
+            "id": 2}]
 
 @app.get('/')
 def root():
@@ -11,10 +25,12 @@ def root():
 
 @app.get('/posts')
 def get_posts():
-    return {'data': 'This is your posts'}
+    return {'data': my_posts}
 
 
-@app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"} 
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict} 
