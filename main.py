242ec7fd33856029c913478bff3f361b5a1ab184
Luciano Filho <lvgalvaofilho@gmail.com>
2023-07-23 15:52:38 -0300

    lint: format main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from random import randrange
+from typing import Optional
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
-from random import randrange
 
 app = FastAPI()
+
 
 class Post(BaseModel):
     title: str
@@ -11,12 +13,18 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-my_posts = [{"title": "title of posts1",
-            "content": "content of post1",
-            "id": 1},
-            {"title": "title of posts2",
-            "content": "content of post2",
-            "id": 2}]
+
+my_posts = [
+    {'title': 'title of posts1', 'content': 'content of post1', 'id': 1},
+    {'title': 'title of posts2', 'content': 'content of post2', 'id': 2},
+]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
 
 @app.get('/')
 def root():
@@ -28,9 +36,22 @@ def get_posts():
     return {'data': my_posts}
 
 
-@app.post("/posts")
+@app.get('/posts/{id}')
+def get_post(id: int):
+    post = find_post(id)
+    return {'data': post}
+
+
+@app.post('/posts')
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
-    return {"data": post_dict} 
+    return {'data': post_dict}
+
+
+@app.delete('/posts/{id}')
+def delete_post(id: int):
+    post = find_post(id)
+    del my_posts[my_posts.index(post)]
+    return {'data': post}
