64c70eea3812c82a56c87c8e4e19801420607a2f
Luciano Filho <lvgalvaofilho@gmail.com>
2023-07-30 02:11:56 -0300

    feat: Replace psycopg2 with SQLAlchemy for database connectivity

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy_utils import create_database, database_exists
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@postgres:5432/curso"
+SQLALCHEMY_DATABASE_URL = (
+    'postgresql://postgres:mysecretpassword@postgres:5432/curso'
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -15,3 +17,11 @@ print(database_exists(engine.url))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
